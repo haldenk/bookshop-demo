1f91c1653a68de312c6f0517fd42b0b2a9d347c6
Halden Kavanagh <haldenkav@gmail.com>
2024-06-05 11:11:24 -0600

    added tailwind.css to site

---
+++ tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "../layouts/_default/baseof.html",
+    "../content/_index.md",
+    "../static/js/script.js",
+  ],
+
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
